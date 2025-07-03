f47635e430ed21c5d3b87094f7cb8193a4da692b
lakshmijyothikali <jyothidevops333@gmail.com>
2025-07-03 20:10:35 +0530

    roboshop shell practice

--- roboshop.sh
+++ roboshop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-09ae3e91bc174bb9b" # replace with your SG ID
+SG_ID="sg-0ac37e2c92d51d162" # replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 ZONE_ID="Z00601042A7NLOUIR8AZ6" # replace with your ZONE ID
 DOMAIN_NAME="jyothiaws84s.site" # replace with your domain
