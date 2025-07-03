ca84b8b1c7e4a13b5ca0ffd0c00df881385f5e4a
lakshmijyothikali <jyothidevops333@gmail.com>
2025-07-03 20:33:48 +0530

    roboshop shell practice

--- roboshop.sh
+++ roboshop.sh
@@ -6,4 +6,16 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z00601042A7NLOUIR8AZ6" # replace with your ZONE ID
 DOMAIN_NAME="jyothiaws84s.site" # replace with your domain
 
-echo "amiid::::$AMI_ID"
+#for instance in ${INSTANCES[@]}
+for instance in $@
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01bc7ebe005fb1cb2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        RECORD_NAME="$DOMAIN_NAME"
+    fi
+    echo "$instance IP address: $IP"
